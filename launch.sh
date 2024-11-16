f5a3e5262bd55d5d243431b98bd07dbdeb594cfc
Pat Erley <paerley@gmail.com>
2024-11-16 00:36:57 -0800

    launch: Update volume location
    The current way I had created the volume caused some issues when
    upgrading winbox.  Resolve by only mounting the volume in the user
    data location.

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xhost +local:
-docker run -it --rm --network=host --ipc=host -e DISPLAY=$DISPLAY --mount source=winbox_winboxdata,target=/winbox/ winbox
+docker run -it --rm --network=host --ipc=host -e DISPLAY=$DISPLAY --mount source=winbox_data,target=/winbox/drive_c/users/wine/AppData/Roaming/Mikrotik/Winbox/ winbox
